9a2d16f2d6f90d096ae1202e2f138c42561d377d
songhyeonpk <ehftozl1997@gmail.com>
2023-10-12 16:34:57 +0900

    [#3] add : 회원 Entity 생성

---
+++ src/main/java/com/shyeon/domain/user/domain/User.java
@@ -0,0 +1,35 @@
+package com.shyeon.domain.user.domain;
+
+import com.shyeon.global.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String nickname;
+
+    @Builder
+    private User(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
