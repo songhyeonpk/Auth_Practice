edb663709a08deea99385ccd4b0fc23f691dffed
songhyeonpk <ehftozl1997@gmail.com>
2023-10-13 14:31:54 +0900

    [#5] add : 패스워드 암호화 객체 생성

---
+++ src/main/java/com/shyeon/global/util/PasswordEncoder.java
@@ -0,0 +1,38 @@
+package com.shyeon.global.util;
+
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+@Component
+public class PasswordEncoder {
+
+    // 패스워드 암호화
+    public String encrypt(String email, String password) {
+        try {
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), getSalt(email), 85319, 128);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException |InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // salt 값 생성
+    private byte[] getSalt(String email) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-512");
+        byte[] keyBytes = email.getBytes("UTF-8");
+
+        return digest.digest(keyBytes);
+    }
+}
