a96c6a903dd5f4d7f08854bb47755681757a60c3
songhyeonpk <ehftozl1997@gmail.com>
2023-10-13 14:50:58 +0900

    [#5] add, feat : UserController 생성 및 회원가입 요청 메소드 추가

---
+++ src/main/java/com/shyeon/domain/user/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.shyeon.domain.user.controller;
+
+import com.shyeon.domain.user.dto.SignupRequestDto;
+import com.shyeon.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    // 회원가입
+    @PostMapping("/api/v1/user")
+    public ResponseEntity<Long> register(@RequestBody @Valid SignupRequestDto request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.register(request));
+    }
+}
