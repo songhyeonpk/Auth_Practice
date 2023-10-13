a61776dcf7b4d70fd3ff4229be00c5dd7d6b515e
songhyeonpk <ehftozl1997@gmail.com>
2023-10-13 12:47:20 +0900

    [#5] add : 회원가입 요청 dto 생성

---
+++ src/main/java/com/shyeon/domain/user/dto/SignupRequestDto.java
@@ -0,0 +1,24 @@
+package com.shyeon.domain.user.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+public class SignupRequestDto {
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "^[a-zA-Z]{6,}[^0-9a-zA-Zㄱ-ㅎ가-힣]{1}$",
+            message = "비밀번호는 6자 이상의 영문으로만 조합하고 마지막에는 특수문자 1개를 포함해야 합니다.")
+    private String password;
+
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[가-힣]{2,4}$", message = "닉네임은 2~4자 이내의 한글이여야 합니다.")
+    private String nickname;
+}
