c6116915b8128fb5db966d04809373789a698565
songhyeonpk <ehftozl1997@gmail.com>
2023-10-18 16:21:55 +0900

    [#7] test : UserService Test 파일 수정 및 로그인 테스트 추가

--- src/test/java/com/shyeon/domain/user/service/UserServiceTest.java
+++ src/test/java/com/shyeon/domain/user/service/UserServiceTest.java
@@ -2,12 +2,16 @@ package com.shyeon.domain.user.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 import com.shyeon.domain.user.domain.User;
+import com.shyeon.domain.user.dto.LoginRequestDto;
+import com.shyeon.domain.user.dto.LoginResponseDto;
 import com.shyeon.domain.user.dto.SignupRequestDto;
 import com.shyeon.domain.user.repository.UserRepository;
 import com.shyeon.global.util.PasswordEncoder;
+import com.shyeon.global.util.jwt.JwtProvider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,78 +21,82 @@ import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.Optional;
+
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
     @Mock private UserRepository userRepository;
 
+    @Mock
     private PasswordEncoder passwordEncoder;
 
-    private UserService userService;
+    @Mock
+    private JwtProvider jwtProvider;
 
-    private SignupRequestDto request;
-
-    @BeforeEach
-    void setUp() {
-        this.passwordEncoder = new PasswordEncoder();
-        this.userService = new UserServiceImpl(userRepository, passwordEncoder);
-
-        this.request = new SignupRequestDto("ehftozl234@naver.com", "thdgus!", "헬로우");
-    }
+    @InjectMocks
+    private UserServiceImpl userService;
 
     @Test
-    @DisplayName("회원가입 시 비밀번호 암호화 테스트")
-    void encryptPassword() {
-        /*
-        given
-        */
-        User user = createTestUserEntity();
-
-        // mocking
-        given(userRepository.save(any(User.class))).willReturn(user);
-
-        /*
-        when
-         */
-        userService.register(request);
-
-        /*
-        then
-         */
-        Assertions.assertThat(user.getPassword())
-                .isEqualTo(passwordEncoder.encrypt(request.getEmail(), request.getPassword()));
-    }
-
-    @Test
-    @DisplayName("회원 생성 성공 테스트")
+    @DisplayName("회원가입 성공 테스트")
     void successCreateUser() {
-        /*
-        given
-         */
-        User user = createTestUserEntity();
+        // given
+        SignupRequestDto request = new SignupRequestDto("ehftozl234@naver.com", "thdgus!", "헬로우");
+
+        User user = User.builder().email(request.getEmail()).password("hashed_password").nickname(request.getNickname()).build();
         ReflectionTestUtils.setField(user, "id", 1L);
 
-        // mocking
+        // stub
+        given(userRepository.existsByEmail(request.getEmail())).willReturn(false);
+        given(userRepository.existsByNickname(request.getNickname())).willReturn(false);
+        given(passwordEncoder.encrypt(request.getEmail(), request.getPassword())).willReturn("hashed_password");
         given(userRepository.save(any(User.class))).willReturn(user);
 
-        /*
-        when
-         */
+        // when
         Long userId = userService.register(request);
 
-        /*
-        then
-         */
+
+        // then
         Assertions.assertThat(user.getId()).isEqualTo(userId);
     }
 
-    private User createTestUserEntity() {
-        String encryptPassword = passwordEncoder.encrypt(request.getEmail(), request.getPassword());
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    void successLogin() {
+        // given
+        LoginRequestDto request = new LoginRequestDto("ehftozl234@naver.com", "thdgus!");
 
-        return User.builder()
-                .email(request.getEmail())
-                .password(encryptPassword)
-                .nickname(request.getNickname())
-                .build();
+        User user = User.builder().email(request.getEmail()).password("hashed_password").build();
+        ReflectionTestUtils.setField(user, "id", 1L);
+
+        // stub
+        given(userRepository.findByEmail(request.getEmail())).willReturn(Optional.of(user));
+        given(passwordEncoder.encrypt(request.getEmail(), request.getPassword())).willReturn("hashed_password");
+        given(jwtProvider.generateAccessToken(request.getEmail())).willReturn("access_token");
+
+        // when
+        LoginResponseDto response = userService.login(request);
+
+        // then
+        Assertions.assertThat(response.getId()).isEqualTo(user.getId());
+        Assertions.assertThat(response.getEmail()).isEqualTo(user.getEmail());
+        Assertions.assertThat(response.getTokens().getAccessToken()).isEqualTo("access_token");
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트")
+    void failLogin() {
+        // given
+        LoginRequestDto request = new LoginRequestDto("ehftozl234@naver.com", "thdgus!");
+
+        User user = User.builder().email("ehftozl234@naver.com").password("hashed_password").build();
+
+        // stub
+        given(userRepository.findByEmail(request.getEmail())).willReturn(Optional.of(user));
+        given(passwordEncoder.encrypt(request.getEmail(), request.getPassword())).willReturn("fake_password");
+        given(jwtProvider.generateAccessToken(request.getEmail())).willReturn("access_token");
+
+        // when
+        LoginResponseDto response = userService.login(request);
     }
 }
