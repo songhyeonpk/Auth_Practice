20098ee0e28e072ebdcf254070004b8e9d156883
songhyeonpk <ehftozl1997@gmail.com>
2023-10-18 16:22:19 +0900

    [#7] test : 로그인 API 테스트 추가

--- src/test/java/com/shyeon/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/shyeon/domain/user/controller/UserControllerTest.java
@@ -8,7 +8,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shyeon.domain.user.domain.User;
+import com.shyeon.domain.user.dto.LoginRequestDto;
+import com.shyeon.domain.user.dto.LoginResponseDto;
 import com.shyeon.domain.user.dto.SignupRequestDto;
+import com.shyeon.domain.user.dto.Tokens;
 import com.shyeon.domain.user.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -55,5 +59,30 @@ class UserControllerTest {
                                 .content(jsonStr))
                 .andExpect(status().isCreated())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("로그인 API 테스트")
+    void loginApiTest() throws Exception {
+        // given
+        LoginRequestDto request = new LoginRequestDto("ehftozl234@naver.com", "thdgus!");
+        String jsonStr = objectMapper.writeValueAsString(request);
+
+        LoginResponseDto response = LoginResponseDto.of(1L, request.getEmail(), "헬로우", Tokens.from("access_token"));
+
+        // stub
+        given(userService.login(any(LoginRequestDto.class))).willReturn(response);
+
+        // when & then
+        mockMvc.perform(post("/api/v1/user/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonStr))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("$.id").value(1L),
+                        jsonPath("$.email").value(request.getEmail()),
+                        jsonPath("$.tokens.accessToken").value("access_token")
+                ).andDo(print());
     }
 }
