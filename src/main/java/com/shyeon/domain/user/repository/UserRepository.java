27bb883875c2c635e60078903f8027581001e8c1
songhyeonpk <ehftozl1997@gmail.com>
2023-10-18 16:19:26 +0900

    [#7] feat : 이메일로 회원을 조회하는 메소드 생성

--- src/main/java/com/shyeon/domain/user/repository/UserRepository.java
+++ src/main/java/com/shyeon/domain/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.shyeon.domain.user.repository;
 import com.shyeon.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // email 중복확인
@@ -10,4 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // nickname 중복확인
     boolean existsByNickname(String nickname);
+
+    // 이메일로 회원조회
+    Optional<User> findByEmail(String email);
 }
