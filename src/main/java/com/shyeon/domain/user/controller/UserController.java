fb0ed7090d606c4a36ac13a16fc92ba0bdc1a8c5
songhyeonpk <ehftozl1997@gmail.com>
2023-10-18 18:07:10 +0900

    [#9] feat : 내 정보 조회 API 생성

--- src/main/java/com/shyeon/domain/user/controller/UserController.java
+++ src/main/java/com/shyeon/domain/user/controller/UserController.java
@@ -3,11 +3,17 @@ package com.shyeon.domain.user.controller;
 import com.shyeon.domain.user.dto.LoginRequestDto;
 import com.shyeon.domain.user.dto.LoginResponseDto;
 import com.shyeon.domain.user.dto.SignupRequestDto;
+import com.shyeon.domain.user.dto.UserInfoResponseDto;
 import com.shyeon.domain.user.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
+import com.shyeon.global.util.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final JwtProvider jwtProvider;
 
     // 회원가입
     @PostMapping("/api/v1/user")
@@ -28,5 +35,12 @@ public class UserController {
     @PostMapping("/api/v1/user/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto request) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.login(request));
+    }
+
+    // 마이페이지 (내 정보 조회)
+    @GetMapping("/api/v1/user/me")
+    public ResponseEntity<UserInfoResponseDto> myInfo(HttpServletRequest request) {
+        String accessToken = jwtProvider.resolveAccessToken(request);
+        return ResponseEntity.status(HttpStatus.OK).body(userService.myInfo(accessToken));
     }
 }
