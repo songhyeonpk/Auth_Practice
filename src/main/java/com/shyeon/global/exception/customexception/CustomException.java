b3d19fae62ae505abe807b3f0a60372aff5735f1
songhyeonpk <ehftozl1997@gmail.com>
2023-10-20 15:00:58 +0900

    [#11] add : CustomExcpetion 생성

---
+++ src/main/java/com/shyeon/global/exception/customexception/CustomException.java
@@ -0,0 +1,15 @@
+package com.shyeon.global.exception.customexception;
+
+import com.shyeon.global.exception.errorcode.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
