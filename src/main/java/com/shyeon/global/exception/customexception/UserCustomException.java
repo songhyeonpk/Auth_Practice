cab6e50b60c191da1b0ed8865dd498a486666463
songhyeonpk <ehftozl1997@gmail.com>
2023-10-20 15:01:26 +0900

    [#11] add : 회원관련 CustomException 생성

---
+++ src/main/java/com/shyeon/global/exception/customexception/UserCustomException.java
@@ -0,0 +1,17 @@
+package com.shyeon.global.exception.customexception;
+
+import com.shyeon.global.exception.errorcode.UserErrorCode;
+import lombok.Getter;
+
+@Getter
+public class UserCustomException extends CustomException {
+
+    public static final UserCustomException NOT_FOUND_USER = new UserCustomException(UserErrorCode.NOT_FOUND_USER);
+    public static final UserCustomException ALREADY_EMAIL = new UserCustomException(UserErrorCode.ALREADY_EMAIL);
+    public static final UserCustomException ALREADY_NICKNAME = new UserCustomException(UserErrorCode.ALREADY_NICKNAME);
+    public static final UserCustomException INVALID_AUTHENTICATION = new UserCustomException(UserErrorCode.INVALID_AUTHENTICATION);
+
+    public UserCustomException(UserErrorCode userErrorCode) {
+        super(userErrorCode);
+    }
+}
