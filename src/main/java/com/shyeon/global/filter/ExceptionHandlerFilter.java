b1cf7e4a56d39eabde9e594e8441371e76ebe9f2
songhyeonpk <ehftozl1997@gmail.com>
2023-10-23 16:19:14 +0900

    [#13] feat : 예외를 처리하는 필터 구현

---
+++ src/main/java/com/shyeon/global/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,39 @@
+package com.shyeon.global.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shyeon.global.exception.ErrorResponse;
+import com.shyeon.global.exception.customexception.TokenCustomException;
+import com.shyeon.global.exception.errorcode.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.io.IOException;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (TokenCustomException e) {
+            setErrorResponse(response, e.getErrorCode());
+        }
+    }
+
+    private void setErrorResponse(HttpServletResponse response, ErrorCode errorCode)throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.setStatus(errorCode.getStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.from(errorCode)));
+    }
+}
