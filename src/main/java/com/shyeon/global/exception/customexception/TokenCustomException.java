5b66f6f5ae30c111fb027a1ca55a30ba884ba861
songhyeonpk <ehftozl1997@gmail.com>
2023-10-23 23:01:03 +0900

    style : spotless

--- src/main/java/com/shyeon/global/exception/customexception/TokenCustomException.java
+++ src/main/java/com/shyeon/global/exception/customexception/TokenCustomException.java
@@ -4,11 +4,16 @@ import com.shyeon.global.exception.errorcode.TokenErrorCode;
 
 public class TokenCustomException extends CustomException {
 
-    public static final TokenCustomException DOES_NOT_EXIST_TOKEN = new TokenCustomException(TokenErrorCode.DOES_NOT_EXIST_TOKEN);
-    public static final TokenCustomException EXPIRED_TOKEN = new TokenCustomException(TokenErrorCode.EXPIRED_TOKEN);
-    public static final TokenCustomException WRONG_TYPE_SIGNATURE = new TokenCustomException(TokenErrorCode.WRONG_TYPE_SIGNATURE);
-    public static final TokenCustomException WRONG_TYPE_TOKEN = new TokenCustomException(TokenErrorCode.WRONG_TYPE_TOKEN);
-    public static final TokenCustomException MALFORMED_TOKEN = new TokenCustomException(TokenErrorCode.MALFORMED_TOKEN);
+    public static final TokenCustomException DOES_NOT_EXIST_TOKEN =
+            new TokenCustomException(TokenErrorCode.DOES_NOT_EXIST_TOKEN);
+    public static final TokenCustomException EXPIRED_TOKEN =
+            new TokenCustomException(TokenErrorCode.EXPIRED_TOKEN);
+    public static final TokenCustomException WRONG_TYPE_SIGNATURE =
+            new TokenCustomException(TokenErrorCode.WRONG_TYPE_SIGNATURE);
+    public static final TokenCustomException WRONG_TYPE_TOKEN =
+            new TokenCustomException(TokenErrorCode.WRONG_TYPE_TOKEN);
+    public static final TokenCustomException MALFORMED_TOKEN =
+            new TokenCustomException(TokenErrorCode.MALFORMED_TOKEN);
 
     public TokenCustomException(TokenErrorCode tokenErrorCode) {
         super(tokenErrorCode);
