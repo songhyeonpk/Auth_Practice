1cd0a8d3a18b0167d6454bdce425b01c4a2e8a61
songhyeonpk <ehftozl1997@gmail.com>
2023-10-24 17:39:30 +0900

    [#15] feat : 내 정보 조회 인터셉터 적용 로직 생성 및 구현

--- src/main/java/com/shyeon/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/shyeon/domain/user/service/UserServiceImpl.java
@@ -101,4 +101,14 @@ public class UserServiceImpl implements UserService {
 
         return UserInfoResponseDto.from(user);
     }
+
+    @Override
+    public UserInfoResponseDto myInfoWithInterceptor(String email) {
+        User user =
+                userRepository
+                        .findByEmail(email)
+                        .orElseThrow(() -> UserCustomException.NOT_FOUND_USER);
+
+        return UserInfoResponseDto.from(user);
+    }
 }
