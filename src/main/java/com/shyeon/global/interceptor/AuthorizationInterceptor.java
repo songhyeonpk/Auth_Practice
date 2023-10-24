a36f33e4c4499ef16bbc48a41891c2dcc0760ab2
songhyeonpk <ehftozl1997@gmail.com>
2023-10-24 17:37:59 +0900

    [#15] feat : 인가 처리 인터셉터 생성 및 로직 구현

---
+++ src/main/java/com/shyeon/global/interceptor/AuthorizationInterceptor.java
@@ -0,0 +1,38 @@
+package com.shyeon.global.interceptor;
+
+import com.shyeon.global.exception.customexception.TokenCustomException;
+import com.shyeon.global.util.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AuthorizationInterceptor implements HandlerInterceptor {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        log.info("AuthorizationInterceptor Run.");
+
+        String accessToken = jwtProvider.resolveAccessToken(request);
+
+        try {
+            if(jwtProvider.validateToken(accessToken)) {
+                String email = jwtProvider.parseClaims(accessToken).getSubject();
+                request.setAttribute("email", email);
+                return true;
+            }
+        } catch (TokenCustomException e) {
+            log.error("code : {}, message : {}", e.getErrorCode().getCode(), e.getErrorCode().getMessage());
+            throw e;
+        }
+
+        return false;
+    }
+
+}
