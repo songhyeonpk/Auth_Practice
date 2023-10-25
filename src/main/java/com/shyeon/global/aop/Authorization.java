9fe1a9d0a7c8a632b65a2dc728c6f90dd6d1bb65
songhyeonpk <ehftozl1997@gmail.com>
2023-10-25 18:19:40 +0900

    [#17] feat : @Authorization 어노테이션에 새로운 메소드 'bindEmail()' 추가

--- src/main/java/com/shyeon/global/aop/Authorization.java
+++ src/main/java/com/shyeon/global/aop/Authorization.java
@@ -8,4 +8,8 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Authorization {
+
+    // 이메일 값 바인딩 필요여부
+    // 인가 과정은 필요하지만 이메일 바인딩 과정은 필요없다면 false
+    boolean bindEmail() default true;
 }
