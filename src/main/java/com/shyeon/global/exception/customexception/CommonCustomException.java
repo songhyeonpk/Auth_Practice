dc67373eda918fa18b8bbdcd23289646a03b08f9
songhyeonpk <ehftozl1997@gmail.com>
2023-10-25 16:55:53 +0900

    [#17] add :공통 커스텀 예외 클래스 생성 및 예외 추가

---
+++ src/main/java/com/shyeon/global/exception/customexception/CommonCustomException.java
@@ -0,0 +1,12 @@
+package com.shyeon.global.exception.customexception;
+
+import com.shyeon.global.exception.errorcode.CommonErrorCode;
+
+public class CommonCustomException extends CustomException {
+
+    public static final CommonCustomException DOES_NOT_EXIST_PARAMETER_IN_METHOD = new CommonCustomException(CommonErrorCode.DOES_NOT_EXIST_PARAMETER_IN_METHOD);
+
+    public CommonCustomException(CommonErrorCode commonErrorCode) {
+        super(commonErrorCode);
+    }
+}
