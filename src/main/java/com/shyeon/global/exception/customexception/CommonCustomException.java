b1d7260fefbb6062f9d0c146901bc04019649331
songhyeonpk <ehftozl1997@gmail.com>
2023-10-26 14:54:22 +0900

    style : spotless

--- src/main/java/com/shyeon/global/exception/customexception/CommonCustomException.java
+++ src/main/java/com/shyeon/global/exception/customexception/CommonCustomException.java
@@ -4,7 +4,8 @@ import com.shyeon.global.exception.errorcode.CommonErrorCode;
 
 public class CommonCustomException extends CustomException {
 
-    public static final CommonCustomException DOES_NOT_EXIST_PARAMETER_IN_METHOD = new CommonCustomException(CommonErrorCode.DOES_NOT_EXIST_PARAMETER_IN_METHOD);
+    public static final CommonCustomException DOES_NOT_EXIST_PARAMETER_IN_METHOD =
+            new CommonCustomException(CommonErrorCode.DOES_NOT_EXIST_PARAMETER_IN_METHOD);
 
     public CommonCustomException(CommonErrorCode commonErrorCode) {
         super(commonErrorCode);
