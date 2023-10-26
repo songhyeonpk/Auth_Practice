fe402d76b2944c36750e2b6568b58cb992b30bd7
songhyeonpk <ehftozl1997@gmail.com>
2023-10-26 14:35:10 +0900

    [#17] feat : UserController에 이메일 바인딩이 필요하지 않는 회원 조회 요청 추가

--- src/main/java/com/shyeon/domain/user/controller/UserController.java
+++ src/main/java/com/shyeon/domain/user/controller/UserController.java
@@ -12,10 +12,7 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -62,5 +59,13 @@ public class UserController {
     @Authorization
     public ResponseEntity<UserInfoResponseDto> myInfoWithAop(String email) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.myInfoWithAop(email));
+    }
+
+    // 회원 조회
+    // 로그인한 회원에 한하여 다른 회원을 조회할 수 있으며, 로그인한 회원의 이메일 정보는 필요없는 요청
+    @GetMapping("/api/v1/user/{userId}/aop")
+    @Authorization(bindEmail = false)
+    public ResponseEntity<UserInfoResponseDto> selectUserWithAop(@PathVariable Long userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.selectUserWithAop(userId));
     }
 }
