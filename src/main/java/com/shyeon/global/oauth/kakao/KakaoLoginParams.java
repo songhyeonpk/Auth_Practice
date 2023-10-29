49ef40ad56113c6711482dfc05738b648d2bed62
songhyeonpk <ehftozl1997@gmail.com>
2023-10-29 18:57:06 +0900

    [#19] feat : 카카오 로그인 시 필요한 정보를 관리하는 KakaoLoginParams 클래스 생성

---
+++ src/main/java/com/shyeon/global/oauth/kakao/KakaoLoginParams.java
@@ -0,0 +1,28 @@
+package com.shyeon.global.oauth.kakao;
+
+import com.shyeon.global.oauth.OAuthLoginParams;
+import com.shyeon.global.oauth.OAuthProvider;
+import lombok.Getter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+public class KakaoLoginParams implements OAuthLoginParams {
+
+    @Getter
+    private String authorizationCode;
+
+    @Override
+    public OAuthProvider getProvider() {
+        return OAuthProvider.KAKAO;
+    }
+
+    @Override
+    public MultiValueMap<String, String> makeRequestBody() {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", this.authorizationCode);
+
+        return body;
+    }
+
+
+}
