6715f425063f0dd2d981031db8742ef6bedef805
songhyeonpk <ehftozl1997@gmail.com>
2023-10-31 15:31:26 +0900

    [#19] feat : 요청한 OAuthProvider을 검사하고 OAuthProvider형으로 변환해주는 convert() 메소드 추가

--- src/main/java/com/shyeon/global/oauth/OAuthProvider.java
+++ src/main/java/com/shyeon/global/oauth/OAuthProvider.java
@@ -1,6 +1,19 @@
 package com.shyeon.global.oauth;
 
+import com.shyeon.domain.oauth.controller.OAuthController;
+import com.shyeon.global.exception.customexception.OAuthCustomException;
+
+import java.util.Arrays;
+
 public enum OAuthProvider {
 
     KAKAO;
+
+    // OAuthProvider 값으로 변환하고, 일치하는 Provider 가 없는 경우 Exception 처리
+    public static OAuthProvider convert(String oAuthProvider) {
+        return Arrays.stream(OAuthProvider.values())
+                .filter(provider -> provider.toString().equals(oAuthProvider.toUpperCase()))
+                .findFirst()
+                .orElseThrow(() -> OAuthCustomException.UNSUPPORTED_OAUTH_LOGIN);
+    }
 }
