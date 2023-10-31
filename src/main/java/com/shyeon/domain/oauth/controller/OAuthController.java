1ab5a44e21d195891c43bb3be263d5c333042417
songhyeonpk <ehftozl1997@gmail.com>
2023-10-31 15:32:38 +0900

    [#19] feat : 소셜 회원가입 요청 API 구현

--- src/main/java/com/shyeon/domain/oauth/controller/OAuthController.java
+++ src/main/java/com/shyeon/domain/oauth/controller/OAuthController.java
@@ -1,19 +1,31 @@
 package com.shyeon.domain.oauth.controller;
 
 import com.shyeon.domain.oauth.dto.OAuthLoginResponseDto;
+import com.shyeon.domain.oauth.dto.OAuthSignupRequestDto;
 import com.shyeon.domain.oauth.service.OAuthService;
 import com.shyeon.global.oauth.kakao.KakaoLoginParams;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class OAuthController {
 
     private final OAuthService oAuthService;
+
+    // OAuth 회원가입 요청
+    @PostMapping("/api/v1/{oAuthProvider}/user")
+    public ResponseEntity<Long> oAuthRegister(@RequestBody @Valid OAuthSignupRequestDto signupRequest, @PathVariable String oAuthProvider) {
+        System.out.println(oAuthProvider);
+        return ResponseEntity.status(HttpStatus.CREATED).body(oAuthService.oAuthRegister(signupRequest, oAuthProvider));
+    }
 
     // 카카오 로그인 요청
     @PostMapping("/api/v1/kakao/login")
