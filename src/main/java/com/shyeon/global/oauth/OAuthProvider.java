8d3d7ae89c0240a3a94220a3e851f2bd4a204631
songhyeonpk <ehftozl1997@gmail.com>
2023-11-01 18:01:08 +0900

    [#21] feat : OAuthProvider enum 데이터에 GOOGLE 추가

--- src/main/java/com/shyeon/global/oauth/OAuthProvider.java
+++ src/main/java/com/shyeon/global/oauth/OAuthProvider.java
@@ -4,7 +4,7 @@ import com.shyeon.global.exception.customexception.OAuthCustomException;
 import java.util.Arrays;
 
 public enum OAuthProvider {
-    KAKAO;
+    KAKAO, GOOGLE;
 
     // OAuthProvider 값으로 변환하고, 일치하는 Provider 가 없는 경우 Exception 처리
     public static OAuthProvider convert(String oAuthProvider) {
