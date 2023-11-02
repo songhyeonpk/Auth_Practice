5746f9446122fe0f6e5505b11630ae80d27325d1
songhyeonpk <ehftozl1997@gmail.com>
2023-11-02 16:52:52 +0900

    style : spotless

--- src/main/java/com/shyeon/domain/oauth/controller/OAuthController.java
+++ src/main/java/com/shyeon/domain/oauth/controller/OAuthController.java
@@ -3,18 +3,13 @@ package com.shyeon.domain.oauth.controller;
 import com.shyeon.domain.oauth.dto.OAuthLoginResponseDto;
 import com.shyeon.domain.oauth.dto.OAuthSignupRequestDto;
 import com.shyeon.domain.oauth.service.OAuthService;
-import com.shyeon.global.oauth.OAuthLoginParams;
 import com.shyeon.global.oauth.google.GoogleLoginParams;
 import com.shyeon.global.oauth.kakao.KakaoLoginParams;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -34,8 +29,10 @@ public class OAuthController {
 
     // 소셜 서비스 Redirect URI
     @GetMapping("/{provider}/callback")
-    public ResponseEntity<String> getRedirectUri(@PathVariable String provider, @RequestParam("code") String code) {
-        return ResponseEntity.status(HttpStatus.OK).body(oAuthService.responseOAuthCode(provider, code));
+    public ResponseEntity<String> getRedirectUri(
+            @PathVariable String provider, @RequestParam("code") String code) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(oAuthService.responseOAuthCode(provider, code));
     }
 
     // 카카오 로그인 요청
@@ -47,7 +44,9 @@ public class OAuthController {
 
     // 구글 로그인 요청
     @PostMapping("/api/v1/google/login")
-    public ResponseEntity<OAuthLoginResponseDto> googleLogin(@RequestBody GoogleLoginParams googleLoginParams) {
-        return ResponseEntity.status(HttpStatus.OK).body(oAuthService.oAuthLogin(googleLoginParams));
+    public ResponseEntity<OAuthLoginResponseDto> googleLogin(
+            @RequestBody GoogleLoginParams googleLoginParams) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(oAuthService.oAuthLogin(googleLoginParams));
     }
 }
