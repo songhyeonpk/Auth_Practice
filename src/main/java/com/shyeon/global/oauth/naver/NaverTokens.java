468267c77ce3532ae79132d52ec67afc0e4c17f4
songhyeonpk <ehftozl1997@gmail.com>
2023-11-02 17:51:30 +0900

    [#23] feat : 네이버 서버로부터 발급 받은 토큰 정보를 관리하는 NaverTokens 클래스 생성

---
+++ src/main/java/com/shyeon/global/oauth/naver/NaverTokens.java
@@ -0,0 +1,22 @@
+package com.shyeon.global.oauth.naver;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NaverTokens {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+}
