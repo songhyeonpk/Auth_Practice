c80c4755ebb0addba670774c1a95029a767841a2
songhyeonpk <ehftozl1997@gmail.com>
2023-11-03 15:46:46 +0900

    [#23] refactor, feat : 인가 데이터를 응답하는 메소드의 이름, 파라미터 값, 반환 값 변경 및 로직 수정

--- src/main/java/com/shyeon/domain/oauth/service/OAuthService.java
+++ src/main/java/com/shyeon/domain/oauth/service/OAuthService.java
@@ -1,13 +1,15 @@
 package com.shyeon.domain.oauth.service;
 
+import com.shyeon.domain.oauth.dto.OAuthAuthorizationDataResponseDto;
 import com.shyeon.domain.oauth.dto.OAuthLoginResponseDto;
+import com.shyeon.domain.oauth.dto.OAuthRedirectDataDto;
 import com.shyeon.domain.oauth.dto.OAuthSignupRequestDto;
 import com.shyeon.global.oauth.OAuthLoginParams;
 
 public interface OAuthService {
 
-    // OAuth 서버로부터 받은 Code 응답
-    String responseOAuthCode(String provider, String code);
+    // OAuth 서버로부터 받은 인가 데이터 응답
+    OAuthAuthorizationDataResponseDto responseAuthData(String provider, OAuthRedirectDataDto data);
 
     // OAuth 로그인
     OAuthLoginResponseDto oAuthLogin(OAuthLoginParams oAuthLoginParams);
