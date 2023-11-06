ed17aff68c7f6ef5c5792204f0a480768add6911
songhyeonpk <ehftozl1997@gmail.com>
2023-11-06 18:31:58 +0900

    [#25] feat : SnsUserRepository 생성

---
+++ src/main/java/com/shyeon/domain/user/repository/SnsUserRepository.java
@@ -0,0 +1,7 @@
+package com.shyeon.domain.user.repository;
+
+import com.shyeon.domain.user.domain.SnsUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SnsUserRepository extends JpaRepository<SnsUser, Long> {
+}
