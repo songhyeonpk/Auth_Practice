e4db379e9844d5aa1c79edc7385c13890e35fc3a
songhyeonpk <ehftozl1997@gmail.com>
2023-11-07 17:01:20 +0900

    [#25] feat : OAuthService에 OAuth 로그인 버전2, OAuth 회원가입 버전2 추가 및 OAuthServiceImpl에서 구현

--- src/main/java/com/shyeon/domain/oauth/service/OAuthServiceImpl.java
+++ src/main/java/com/shyeon/domain/oauth/service/OAuthServiceImpl.java
@@ -1,11 +1,16 @@
 package com.shyeon.domain.oauth.service;
 
 import com.shyeon.domain.oauth.dto.*;
+import com.shyeon.domain.user.domain.SnsUser;
 import com.shyeon.domain.user.domain.User;
+import com.shyeon.domain.user.domain.UserV2;
 import com.shyeon.domain.user.dto.Tokens;
+import com.shyeon.domain.user.repository.SnsUserRepository;
 import com.shyeon.domain.user.repository.UserRepository;
+import com.shyeon.domain.user.repository.UserV2Repository;
 import com.shyeon.global.exception.customexception.UserCustomException;
 import com.shyeon.global.oauth.*;
+import com.shyeon.global.util.PasswordEncoder;
 import com.shyeon.global.util.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +21,10 @@ public class OAuthServiceImpl implements OAuthService {
 
     private final RequestOAuthApiClientService requestOAuthApiClientService;
     private final UserRepository userRepository;
+    private final UserV2Repository userV2Repository;
+    private final SnsUserRepository snsUserRepository;
     private final JwtProvider jwtProvider;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public OAuthAuthorizationDataResponseDto responseAuthData(
@@ -72,6 +80,69 @@ public class OAuthServiceImpl implements OAuthService {
                         .build();
 
         return userRepository.save(user).getId();
+    }
+
+    @Override
+    public OAuthLoginResponseDto oAuthLoginV2(OAuthLoginParams oAuthLoginParams) {
+        OAuthInfoResponse response = requestOAuthApiClientService.requestOAuth(oAuthLoginParams);
+
+        return responseOAuthLoginV2(response);
+    }
+
+    private OAuthLoginResponseDto responseOAuthLoginV2(OAuthInfoResponse response) {
+        UserV2 user = userV2Repository.findUserBySnsUser(response.getOAuthProvider(), response.getOAuthId());
+
+        // 로그인 진행, JWT 발급
+        if(user != null) {
+            String accessToken = jwtProvider.generateAccessToken(user.getEmail());
+
+            return OAuthLoginSuccessResponseDtoV2.of(true, user, Tokens.from(accessToken));
+        }
+
+        return OAuthLoginFailResponseDto.of(false, response, "소셜 간편 회원가입이 필요합니다.");
+    }
+
+    @Override
+    public Long oAuthRegisterV2(OAuthSignupRequestDtoV2 signupRequest) {
+        // 이메일 중복확인
+        existsEmail(signupRequest.getEmail());
+
+        // 닉네임 중복확인
+        existsNickname(signupRequest.getNickname());
+
+        // 소셜 제공자 검증 및 타입 변환
+        OAuthProvider provider = OAuthProvider.convert(signupRequest.getOauthProvider());
+
+        // SnsUser 생성
+        SnsUser snsUser = SnsUser.builder()
+                .oAuthProvider(provider)
+                .oAuthId(signupRequest.getOauthId())
+                .build();
+
+        // SnsUser 저장
+        snsUserRepository.save(snsUser);
+
+        // 비밀번호 암호화
+        String encryptPassword = passwordEncoder.encrypt(signupRequest.getEmail(), signupRequest.getPassword());
+
+        // User 생성
+        UserV2 user = UserV2.builder()
+                .email(signupRequest.getEmail())
+                .password(encryptPassword)
+                .nickname(signupRequest.getNickname())
+                .build();
+
+        // user 정보에 Sns 정보 셋팅
+        user.setSnsUser(snsUser);
+
+        return userV2Repository.save(user).getId();
+    }
+
+    // 이메일 중복 확인
+    private void existsEmail(String email) {
+        if(userRepository.existsByEmail(email)) {
+            throw UserCustomException.ALREADY_EMAIL;
+        }
     }
 
     // 닉네임 중복 확인
