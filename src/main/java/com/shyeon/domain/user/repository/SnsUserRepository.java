710271b481e9381ebdc298d4e73a2687d48a2d0c
songhyeonpk <ehftozl1997@gmail.com>
2023-11-07 17:06:16 +0900

    style : spotless

--- src/main/java/com/shyeon/domain/user/repository/SnsUserRepository.java
+++ src/main/java/com/shyeon/domain/user/repository/SnsUserRepository.java
@@ -3,5 +3,4 @@ package com.shyeon.domain.user.repository;
 import com.shyeon.domain.user.domain.SnsUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SnsUserRepository extends JpaRepository<SnsUser, Long> {
-}
+public interface SnsUserRepository extends JpaRepository<SnsUser, Long> {}
