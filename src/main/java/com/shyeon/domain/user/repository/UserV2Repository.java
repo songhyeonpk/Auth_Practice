8199ace9dac63c5365564ed6da838b1a3a3dc874
songhyeonpk <ehftozl1997@gmail.com>
2023-11-07 16:59:53 +0900

    [#25] feat : UserV2 레파지토리 생성 및 SNS 정보로 회원 조회하는 findUserBySnsUser() 추가

---
+++ src/main/java/com/shyeon/domain/user/repository/UserV2Repository.java
@@ -0,0 +1,13 @@
+package com.shyeon.domain.user.repository;
+
+import com.shyeon.domain.user.domain.UserV2;
+import com.shyeon.global.oauth.OAuthProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserV2Repository extends JpaRepository<UserV2, Long> {
+
+    // SNS 정보로 회원 조회
+    @Query("select u from UserV2 u join u.snsUser s where s.oAuthProvider = :oAuthProvider and s.oAuthId = :oAuthId")
+    UserV2 findUserBySnsUser(OAuthProvider oAuthProvider, String oAuthId);
+}
